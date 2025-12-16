df5c8fdb0e4a35c1cecc82b1d193dcbcdf517ebc
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2025-12-16 18:40:27 +0100

    add custom background image and blink animation to Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'header': 'url(/images/bg.jpg)'
+      },
+      animation: {
+        blink: 'blink 1s ease-in-out infinite'
+      },
+      keyframes: {
+        blink: {
+          '0%, 100%': {
+            opacity: '1'
+          },
+          '50%': {
+            opacity: '0.5'
+          }
+        }
+      }
+    },
   },
   plugins: [],
 }
