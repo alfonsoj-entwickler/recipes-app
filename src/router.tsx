f90fcc946a07877ae1761bf8e882de793aedd54d
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2026-01-03 13:03:15 +0100

    implement lazy loading for FavoritesPage with Suspense fallback

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,10 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import IndexPage from "./pages/IndexPage";
-import FavoritesPage from "./pages/FavoritesPage";
+import GenerateAI from "./pages/GenerateAI";
 import MainLayout from "./layouts/MainLayout";
+
+const FavoritesPage = lazy(() => import("./pages/FavoritesPage"));
 
 export default function AppRouter() {
   return (
@@ -9,7 +12,15 @@ export default function AppRouter() {
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/" element={<IndexPage />} index />
-          <Route path="/favorites" element={<FavoritesPage />} />
+          <Route
+            path="/favorites"
+            element={
+              <Suspense fallback="Loading ....">
+                <FavoritesPage />
+              </Suspense>
+            }
+          />
+          <Route path="/generate" element={<GenerateAI />} index />
         </Route>
       </Routes>
     </BrowserRouter>
