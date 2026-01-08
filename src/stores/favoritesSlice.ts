3097efd2ee512b12d8fcfe7d94bebb897167f137
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2026-01-08 06:39:41 +0100

    add favorites slice for managing user favorite recipes

---
+++ src/stores/favoritesSlice.ts
@@ -0,0 +1,37 @@
+import type { StateCreator } from 'zustand';
+import type { Recipe } from '../types';
+
+export type FavoritesSliceType = {
+    favorites: Recipe[]
+    handleClickFavorite: (recipe: Recipe) => void
+    favoriteExists: (id: Recipe['idDrink']) => boolean
+    loadFromStorage: () => void
+}
+
+export const createFavoritesSlice: StateCreator<FavoritesSliceType> = (set, get) => ({
+    favorites: [],
+    handleClickFavorite: (recipe) => {
+        if (get().favoriteExists(recipe.idDrink)) {
+            set((state) => ({
+                favorites: state.favorites.filter(favorite => favorite.idDrink !== recipe.idDrink)
+            }))
+        }
+        else {
+            set((state) => ({
+                favorites: [...state.favorites, recipe]
+            }))
+        }
+        localStorage.setItem('favorites', JSON.stringify(get().favorites))
+    },
+    favoriteExists: (id) => {
+        return get().favorites.some(favorite => favorite.idDrink === id)
+    },
+    loadFromStorage: () => {
+        const storedFavorites = localStorage.getItem('favorites')
+        if (storedFavorites) {
+            set({
+                favorites: JSON.parse(storedFavorites)
+            })
+        }
+    }
+})
