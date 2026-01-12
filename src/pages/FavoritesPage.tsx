37d81b4e51d8c87ed23f50d423269809bfbc3d78
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2026-01-12 18:40:07 +0100

    add FavoritesPage component with enhanced layout and conditional rendering for favorites

--- src/pages/FavoritesPage.tsx
+++ src/pages/FavoritesPage.tsx
@@ -1,7 +1,22 @@
+import { useMemo } from "react";
+import Drinkcard from "../components/Drinkcard";
+import { useAppStore } from "../stores/useAppStore";
+
 export default function FavoritesPage() {
+  const favorites = useAppStore((state) => state.favorites);
+  const isFavorites = useMemo(() => favorites.length > 0, [favorites]);
   return (
     <>
-      <h1>Favorites</h1>
+      <h1 className="text-6xl font-extrabold">Favorites</h1>
+      {isFavorites ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10 gap-10 ">
+          {favorites.map((recipe) => (
+            <Drinkcard key={`favorite-item-${recipe.idDrink}`} drink={recipe} />
+          ))}
+        </div>
+      ) : (
+        <p className="my-10 text-center text-lg font-normal">Empty</p>
+      )}
     </>
   );
 }
