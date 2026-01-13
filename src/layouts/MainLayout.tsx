28c3d9aad043269e4e301e6428e6d5b27f3f246a
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2026-01-13 20:16:39 +0100

    add MainLayout component with modal and notification integration

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,12 +1,24 @@
 import Header from "../components/Header";
+import Notification from "../components/Notification";
 import { Outlet } from "react-router-dom";
+import Modal from "../components/Modal";
+import { useEffect } from "react";
+import { useAppStore } from "../stores/useAppStore";
 export default function MainLayout() {
+  const loadFromStorage = useAppStore((state) => state.loadFromStorage);
+
+  useEffect(() => {
+    loadFromStorage();
+  }, []);
+
   return (
     <>
       <Header />
       <main className="container mx-auto py-16">
         <Outlet />
       </main>
+      <Modal />
+      <Notification />
     </>
   );
 }
